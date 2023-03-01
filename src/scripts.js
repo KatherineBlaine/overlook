2d7460612842ca2d47134270e21bbfc4d846480e
Katherine Blaine <blaine.katherine12@gmail.com>
2023-02-28 18:29:19 -0700

    feat: completed API calls (no error handling yet)

--- src/scripts.js
+++ src/scripts.js
@@ -7,10 +7,29 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import fetchAllData from '../api-calls';
+import User from '../classes/user-class';
+import Booking from '../classes/bookings-class';
+import Room from '../classes/rooms-class';
+
+let user, bookings, rooms;
 let view = 'main';
 const navButton = document.getElementById('nav-button');
 const mainPage = document.getElementById('main-page');
 const userDashboard = document.getElementById('user-dashboard');
+const userGreeting = document.getElementById('user-greeting')
+
+window.addEventListener('load', () => {
+  fetchAllData()
+    .then(data => {
+      const userData = data[0].customers;
+      const roomData = data[1].rooms;
+      const bookingData = data[2].bookings;
+      user = new User(userData[0])
+      bookings = bookingData.map(booking => new Booking(booking))
+      rooms = roomData.map(room => new Room(room))
+    })
+})
 
 navButton.addEventListener('click', () => {
   if (view === 'main') {
